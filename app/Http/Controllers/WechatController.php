4177b00857c3194005e9428498a75bb6770eb391
moronghui <1570987718@qq.com>
2016-09-01 09:14:04 +0800

    user6

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -27,7 +27,7 @@ class WechatController extends Controller
                 case 'text':
                     $users=$user->lists();
                     if ($users!=''&&!is_null($users)) {
-                        return '用户个数：'.$users->data->openid[0];
+                        return '用户个数：'.count($user->data->openid).gettype($user->data->openid);
                     }
                     else{
                         return '无关注用户';
